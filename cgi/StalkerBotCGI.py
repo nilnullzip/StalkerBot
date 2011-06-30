76f2d3f4865ce599ac3b60d2b158ea68010f4fe4
Juan Pineda <juan@2pi.local>
2011-06-30 15:21:45 -0700

    Added logging to log.txt.

--- cgi/StalkerBotCGI.py
+++ cgi/StalkerBotCGI.py
@@ -10,33 +10,24 @@ import os, sys
 sys.path.append("../src")
 
 import commentAndIdToTagSentiment as TS
-#from commentAndIdToTagSentiment import commentAndIdToTagSentiment as TS
 
 os.chdir("../static")
 
-#print s.path
-#if (s.path.startswith("/stalk?userid=")) :
-if ((not os.environ["QUERY_STRING"].startswith("userid=")) and False) :
-    sys.exit(0)
-else :
-#if (False) :
-    #s.send_response(200)
-    #print "Date: Tue, 28 Jun 2011 09:24:44 GMT"
-    #print "Expires: -1"
-    #print "Cache-Control: private, max-age=0"
-#    print "HTTP/1.0 200 OK"
+q = os.environ["QUERY_STRING"]
+
+import datetime
+LOG = open ("../log.txt", "a")
+LOG.write("[%s] URL: %s\n" % (datetime.datetime.today(), q))
+
+if (q.startswith("userid=")) :
     print "Content-Type: text/html"
     print "Status: 200 OK"
     print
-    #print "<html>"
-    #print "<center>Hello, Linux.com!</center>"
-    #print "</html>"
 
-    q = os.environ["QUERY_STRING"]
-#    print "QUERY=%s" % q
-#            s.wfile.write('[["Rock and roll", ["Confidence", "Anxiety", "Compassion", "Hostility", "Depression", "Happiness"]]]')
     ignore, userid = q.split('userid=')
- #   print "ignoring %s" % ignore
- #   print "Stalking: %s" % userid
+
+    LOG.write("[%s] Stalking: %s\n" % (datetime.datetime.today(), userid))
     print TS.getUserTopicSentiments(userid)
- #   print "Done stalking."
+    LOG.write("[%s] Done stalking: %s\n" % (datetime.datetime.today(), userid))
+
+LOG.close()
