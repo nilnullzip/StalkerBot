bfd2b35de118982be2b1c9bfd8a68fadefb5f25b
Juan Pineda <juan@2pi.local>
2011-06-30 18:55:37 -0700

    Cleaned up logging in StalkerBot.py server

--- src/StalkerBot.py
+++ src/StalkerBot.py
@@ -13,25 +13,29 @@ else:
 
 os.chdir("../static")
 
+logging = True
+
+def log (msg) :
+    if logging :
+        LOG = open ("../StalkerBot.log", "a")
+        LOG.write(msg)
+        LOG.close()
+        sys.stderr.write (msg)
+
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(s) :
         import datetime
-        LOG = open ("../log.txt", "a")
-        print s.path
-        LOG.write("[%s] URL: %s\n" % (datetime.datetime.today(), s.path))
+        log("[%s] URL: %s\n" % (datetime.datetime.today(), s.path))
         if (s.path.startswith("/cgi-bin/StalkerBotCGI.py?userid=")) :
             s.send_response(200)
             s.send_header("Content-type", "text/html")
             s.end_headers()
             ignore, userid = s.path.split('userid=')
-            print "Stalking: %s" % userid
-            LOG.write("[%s] Stalking: %s\n" % (datetime.datetime.today(), userid))
+            log("[%s] Stalking: %s\n" % (datetime.datetime.today(), userid))
             s.wfile.write(TS.getUserTopicSentiments(userid))
-            print "Done stalking."
-            LOG.write("[%s] Done stalking: %s\n" % (datetime.datetime.today(), userid))
+            log("[%s] Done stalking: %s\n" % (datetime.datetime.today(), userid))
         else :
             SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(s)
-        LOG.close()
         return
 
 Handler = MyHandler
