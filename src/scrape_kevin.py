3db27061ce87134e0998c3cc5aca80339ddfcc1e
itsmeknt <itsmeknt@gmail.com>
2011-07-06 00:53:51 -0700

    Updated scrape_kevin to only scrape top level comments

--- src/scrape_kevin.py
+++ src/scrape_kevin.py
@@ -1,4 +1,3 @@
-
 import urllib, re, time
 from BeautifulSoup import BeautifulSoup
 
@@ -22,34 +21,29 @@ def scrape(userID):
     nexturl = 'http://news.ycombinator.com/threads?id=' + userID
     commentfieldre = re.compile('<a href="user\?id=' + userID + '">' + userID + '<(.*?)></td>', re.DOTALL)
     commentre = re.compile('class="comment"><font color=.......>(.*?)</fo', re.DOTALL)
-    postIDre = re.compile('parent</a> \| on: <a href="item\?id=(.*)">')
+    postIDre = re.compile('<a href="item\?id=(.*)">parent</a> \| on: <a href="item\?id=(\\1)">')
     nexturlre = re.compile('class="title"><a href="(.*)" rel')
 
-    count = 0
-    
-    # Following commented by DAS on 6/27 to just get one page of comments
-    #while (nexturl):
-    content = urllib.urlopen(nexturl).read()
-    allcommentfield = commentfieldre.findall(content)
-    for currfield in allcommentfield:
-        commentmatch = commentre.search(currfield)
-        comment = convert(sanitize_html(commentmatch.group(1)))
-        postIDmatch = postIDre.search(currfield)
-        if postIDmatch:
-            postID = postIDmatch.group(1)
-        result.append([postID, comment])
-        # Following commented by DAS on 6/27 to just get one page of comments
-        #nexturlmatch = nexturlre.search(content)
-        #if nexturlmatch:
-        #    nexturlappend = nexturlmatch.group(1)
-        #    nexturl = 'http://news.ycombinator.com/' + nexturlappend[1:]
-        #else:
-        #    nexturl = []
-        # inserted by DAS on 6/27 to prevent overloading the server
-        #time.sleep(.05)
-    # return first 10 elements for consistency with HN API
+    while (nexturl and len(result) < 10):
+        content = urllib.urlopen(nexturl).read()
+        allcommentfield = commentfieldre.findall(content)
+        for currfield in allcommentfield:
+            postIDmatch = postIDre.search(currfield)
+            if postIDmatch:
+                postID = postIDmatch.group(1)
+                commentmatch = commentre.search(currfield)
+                comment = convert(sanitize_html(commentmatch.group(1)))
+                result.append([postID, comment])        
+                nexturlmatch = nexturlre.search(content)
+                if nexturlmatch:
+                    nexturlappend = nexturlmatch.group(1)
+                    nexturl = 'http://news.ycombinator.com/' + nexturlappend[1:]
+                else:
+                    nexturl = []
+                time.sleep(.05)
+    print len(result)
     return result
-    #return result
+
 
 #commentIdStructure = scrape("pg")
 #for elt in commentIdStructure:
