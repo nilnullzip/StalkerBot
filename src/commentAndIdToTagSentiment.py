2a97600dcbaf850f7bd86f5080525d089b93f950
Daniel Steinberg <daniel.steinberg@dannyadam.com>
2011-07-14 20:16:58 -0700

    tag and sentiment in parallel fetched in parallel. Passes an updated data structure to the JS: ['tag', ['sentiment1'(,'sentiment2',...)], 'comment', 'threadUrl', 'articleUrl']

--- src/commentAndIdToTagSentiment.py
+++ src/commentAndIdToTagSentiment.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
 
-import json, threading, time
+import json, threading, time, urllib, re
+from urlparse import urlparse
 
 #from scrape_juan import scrape
 from scrape_juan import scrape as scrape_juan
 from scrape_kevin import scrape as scrape_kevin
-from tag_generation import generateTagsFromThreadId
+from tag_generation import generateTags
 from effectcheck_api import getSentiment
+
+# Takes in a Hacker News Thread ID String
+def hnThreadIdToArticleUrl( threadId ):
+    url = "http://news.ycombinator.com/item?id=" + threadId
+    urlConn = urllib.urlopen(url)
+    siteHtml = urlConn.read()
+    urlConn.close()
+    reg = re.compile('<td class="title"><a href="([^"]*)">')
+    regSearch = reg.search(siteHtml)
+    if regSearch == None:
+        return None
+    link = regSearch.group(1)
+    parsedLink = urlparse(link)
+    # if the link is back to the current page, return None
+    if link == "item?id=" + threadId:
+        return None
+    else:
+        return link
 
 def getMaxSentiments(sentimentDict):
     maximum = 0
@@ -26,87 +45,46 @@ def scrape(userID):
 # commentIdStructure is a list of lists. The inner lists contain a String with a ThreadId
 # and the second element has the corresponding comment
 def commentAndIdToTagSentiment(commentIdStructure):
-    tagSentimentOutput = {}
-    tagCount = {}
     threadTags = {}
     threadTagsChecked = [] # List for tracking whether for generating a tag was made yet
+    tagSentUrlComment = []
     
     class GetTags ( threading.Thread ):
         # Override Thread's __init__ method to accept the parameters needed:
-        def __init__ ( self, threadId ):
+        def __init__ ( self, threadId, comment ):
             self.curThreadId = threadId
+            self.curThreadUrl = "http://news.ycombinator.com/item?id=" + self.curThreadId
+            self.curComment = comment
+            self.curArticleUrl = hnThreadIdToArticleUrl(self.curThreadId)
             threading.Thread.__init__ ( self )
     
         def run ( self ):
-            threadTags[self.curThreadId] = generateTagsFromThreadId(self.curThreadId)
-            threadTagsChecked.append(self.curThreadId)
+            # Sleep before first call in case hacker news was called by scrape (scrape_kevin)
+            time.sleep(.05)
+            # No need to generate the same tags over and over
+            if (not (threadId in threadTagsChecked)):
+                threadTags[self.curThreadId] = generateTags(self.curArticleUrl)
+                threadTagsChecked.append(self.curThreadId)
             tags = threadTags[self.curThreadId]
-            # print for testing
-            #print tags
+            
+            sentiment = getSentiment(self.curComment)
+            maxSentiments = getMaxSentiments(sentiment)
             for tag in tags:
-                tagSentimentOutput[tag] = {'Confidence': 0.0, 'Anxiety': 0.0, 'Compassion': 0.0, 'Hostility': 0.0, 'Depression': 0.0, 'Happiness': 0.0}
+                tagSentUrlComment.append([tag, maxSentiments, self.curComment, self.curThreadUrl, self.curArticleUrl])
     
-    counter = 0
     for commentIdElt in commentIdStructure:
-        # print For testing
-        #print commentIdElt
         threadId = commentIdElt[0]
+        comment = commentIdElt[1]
         # No need to generate the same tags over and over
         if (not (threadId in threadTagsChecked)):
             # Sleep before first call in case hacker news was called by scrape (scrape_kevin)
             time.sleep(.05)
-            GetTags ( threadId ).start()
-            # Now the code always sleeps a bit between requests
-            #if (counter >= 5):
-            #    # pause requests after every few requests to prevent overloading server
-            #    time.sleep(.05)
-            #    counter = 0
+            GetTags ( threadId, comment ).start()
                 
-    threadTest = False # Change to True to show thread count
-    
     while (threading.active_count() > 1):
-        if (threadTest):
-            time.sleep(1)
-            print "Thread Count: %s" % threading.active_count()
         pass
-    
-    # tests
-    #print "threads 1 done"
-    #print threadTags
-    #print threadTagsChecked
                 
-    # Generate tag counts
-    for commentIdElt in commentIdStructure:
-        threadId = commentIdElt[0]
-        tags = threadTags[threadId]
-        for tag in tags:
-            if tag in tagCount:
-                tagCount[tag] = tagCount[tag] + 1
-            else:
-                tagCount[tag] = 1
-    
-    for commentIdElt in commentIdStructure:
-        threadId = commentIdElt[0]
-        tags = threadTags[threadId]
-        sentiment = getSentiment(commentIdElt[1])
-        for tag in tags:
-            for k, s in sentiment.iteritems():
-                tagSentimentOutput[tag][k] = tagSentimentOutput[tag][k]+s
-
-    tagAndMaxSentiment = {}
-    for tag in tagSentimentOutput:
-        tagAndMaxSentiment[tag] = getMaxSentiments(tagSentimentOutput[tag])
-
-    sortedCount = sorted(tagCount.items(), key=lambda x: x[1], reverse=True)
-
-    dictToReturn = []
-    for elt in sortedCount:
-        dictToReturn.append([elt[0], tagAndMaxSentiment[elt[0]]])
-        
-    # Future Update: Sort the JSON output by tag appearance
-    #print tagCount
-    #print json.dumps(dictToReturn)
-    return json.dumps(dictToReturn)
+    return json.dumps(tagSentUrlComment)
     
 def getUserTopicSentiments(userid):
     #print userid
