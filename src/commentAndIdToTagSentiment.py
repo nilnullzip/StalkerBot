4d60cc13b4119be56dc53459aa7c6da5a0f4f4f2
Daniel Steinberg <daniel.steinberg@dannyadam.com>
2011-07-21 18:07:02 -0700

    Added caching optimizations. Comments are cached in such a way that a user's comments won't be scraped more than once per day.

--- src/commentAndIdToTagSentiment.py
+++ src/commentAndIdToTagSentiment.py
@@ -1,34 +1,48 @@
 #!/usr/bin/python
 
-import json, threading, time, urllib, re, os
-from urlparse import urlparse
+import json, threading, time, datetime, urllib, re, os
 
-#from scrape_juan import scrape
-from scrape_juan import scrape as scrape_juan
 from scrape_kevin import scrape as scrape_kevin
 from tag_generation import generateTags
 from effectcheck_api import getSentiment
 
 TAG_CACHE_DIRECTORY = "../cache/tags"
 SENTIMENT_CACHE_DIRECTORY = "../cache/sentiments"
+ARTICLE_URL_CACHE_DIRECTORY = "../cache/article-urls"
+SCRAPED_COMMENTS_CACHE_DIRECTORY = "../cache/scraped-comment-data"
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
 
 # Takes in a Hacker News Thread ID String
 def hnThreadIdToArticleUrl( threadId ):
-    url = "http://news.ycombinator.com/item?id=" + threadId
-    urlConn = urllib.urlopen(url)
-    siteHtml = urlConn.read()
-    urlConn.close()
-    reg = re.compile('<td class="title"><a href="([^"]*)">')
-    regSearch = reg.search(siteHtml)
-    if regSearch == None:
-        return None
-    link = regSearch.group(1)
-    parsedLink = urlparse(link)
-    # if the link is back to the current page, return None
-    if link == "item?id=" + threadId:
-        return None
+    urlCacheFileStr = ARTICLE_URL_CACHE_DIRECTORY + "/" + threadId
+    ensure_dir(urlCacheFileStr)
+    if (os.path.isfile(urlCacheFileStr)):
+        cache = open(urlCacheFileStr, "r")
+        articleUrl = json.loads(cache.read())
     else:
-        return link
+        url = "http://news.ycombinator.com/item?id=" + threadId
+        urlConn = urllib.urlopen(url)
+        siteHtml = urlConn.read()
+        urlConn.close()
+        reg = re.compile('<td class="title"><a href="([^"]*)">')
+        regSearch = reg.search(siteHtml)
+        if regSearch == None:
+            return None
+        link = regSearch.group(1)
+        # if the link is back to the current page, return None
+        if link == "item?id=" + threadId:
+            articleUrl = None
+        else:
+            articleUrl = link
+        # Cache
+        cache = open(urlCacheFileStr, "w")
+        cache.write(json.dumps(articleUrl))
+    cache.close()
+    return articleUrl    
 
 def getMaxSentiments(sentimentDict):
     maximum = 0
@@ -54,70 +68,101 @@ def commentAndIdToTagSentiment(commentIdStructure):
     
     class GetTags ( threading.Thread ):
         # Override Thread's __init__ method to accept the parameters needed:
-        def __init__ ( self, threadId, comment, commentId ):
+        def __init__ ( self, threadId, comment, commentId, threadIdLock ):
             self.curThreadId = threadId
             self.curThreadUrl = "http://news.ycombinator.com/item?id=" + self.curThreadId
             self.curComment = comment
-            self.curArticleUrl = hnThreadIdToArticleUrl(self.curThreadId)
             self.curCommentId = commentId
+            self.threadIdLock = threadIdLock
             threading.Thread.__init__ ( self )
     
         def run ( self ):
-            if (self.curArticleUrl != None):
-                # No need to generate the same tags over and over
-                if (not (threadId in threadTagsChecked)):
-                    threadTagsChecked.append(self.curThreadId)
-                    tagCacheFileStr = TAG_CACHE_DIRECTORY + "/" + urllib.quote(self.curThreadId,'')
-                    if (os.path.isfile(tagCacheFileStr)):
-                        cache = open(tagCacheFileStr, "r")
-                        threadTags[self.curThreadId] = json.loads(cache.read())
-                    else:
-                        # Sleep before call in case hacker news was called prior
-                        time.sleep(.05)
-                        threadTags[self.curThreadId] = generateTags(self.curArticleUrl)
-                        # Cache article tags
-                        cache = open(tagCacheFileStr, "w")
-                        cache.write(json.dumps(threadTags[self.curThreadId]))
-                    cache.close()
+            # No need to generate the same tags over and over
+            with self.threadIdLock:
+                self.curArticleUrl = hnThreadIdToArticleUrl(self.curThreadId)
+                if self.curArticleUrl == None:
+                    return
                 else:
-                    # Wait until the tags are added for the threadId by some other thread call
-                    while (not (self.curThreadId in threadTags)):
-                        pass
-                    
-                tags = threadTags[self.curThreadId]
-                
-                sentCacheFileStr = SENTIMENT_CACHE_DIRECTORY + "/" + urllib.quote(self.curCommentId,'')
-                if (os.path.isfile(sentCacheFileStr)):
-                    cache = open(sentCacheFileStr, "r")
-                    maxSentiments = json.loads(cache.read())
-                else:
-                    sentiment = getSentiment(self.curComment)
-                    maxSentiments = getMaxSentiments(sentiment)
-                    # Cache max sentiments
-                    cache = open(sentCacheFileStr, "w")
-                    cache.write(json.dumps(maxSentiments))
-                
-                for tag in tags:
-                    structForJS = [tag, maxSentiments, self.curComment, self.curThreadUrl, self.curArticleUrl]
-                    tagSentUrlComment.append(structForJS)
+                    if (not (self.curThreadId in threadTagsChecked)):
+                        threadTagsChecked.append(self.curThreadId)
+                        tagCacheFileStr = TAG_CACHE_DIRECTORY + "/" + self.curThreadId
+                        ensure_dir(tagCacheFileStr)
+                        if (os.path.isfile(tagCacheFileStr)):
+                            cache = open(tagCacheFileStr, "r")
+                            threadTags[self.curThreadId] = json.loads(cache.read())
+                        else:
+                            # Sleep before call in case hacker news was called prior
+                            time.sleep(.05)
+                            threadTags[self.curThreadId] = generateTags(self.curArticleUrl)
+                            # Cache article tags
+                            cache = open(tagCacheFileStr, "w")
+                            cache.write(json.dumps(threadTags[self.curThreadId]))
+                        cache.close()
+            
+            tags = threadTags[self.curThreadId]
+            
+            sentCacheFileStr = SENTIMENT_CACHE_DIRECTORY + "/" + self.curCommentId
+            ensure_dir(sentCacheFileStr)
+            if (os.path.isfile(sentCacheFileStr)):
+                cache = open(sentCacheFileStr, "r")
+                maxSentiments = json.loads(cache.read())
+            else:
+                sentiment = getSentiment(self.curComment)
+                maxSentiments = getMaxSentiments(sentiment)
+                # Cache max sentiments
+                cache = open(sentCacheFileStr, "w")
+                cache.write(json.dumps(maxSentiments))
+            cache.close()
+            
+            for tag in tags:
+                structForJS = [tag, maxSentiments, self.curComment, self.curThreadUrl, self.curArticleUrl]
+                tagSentUrlComment.append(structForJS)
+    
+    threadList = []
+    threadIdLocks = {}
     
     for commentIdElt in commentIdStructure:
         threadId = commentIdElt[0]
         comment = commentIdElt[1]
         commentId = commentIdElt[2]
-        GetTags ( threadId, comment, commentId ).start()
-                
-    while (threading.active_count() > 1):
-        pass
+        if (not (threadId in threadIdLocks)):
+            threadIdLocks[threadId] = threading.Lock()
+        threadIdLock = threadIdLocks[threadId]
+        threadList.append(GetTags ( threadId, comment, commentId, threadIdLock ))
+        
+    # Start Threads
+    for thread in threadList:
+        thread.start()
+    
+    # Wait for threads to finish      
+    for thread in threadList: 
+        thread.join()
                 
     return json.dumps(tagSentUrlComment)
 
 def getUserTopicSentiments(userid):
-    return commentAndIdToTagSentiment(scrape(userid))
+    # Only scrape once per day
+    today = datetime.date.today()
+    todaystr = today.isoformat()
+
+    scrapedCommentsCacheFileStr = SCRAPED_COMMENTS_CACHE_DIRECTORY + "/" + todaystr + "/" + userid
+    ensure_dir(scrapedCommentsCacheFileStr)
+    
+    if (os.path.isfile(scrapedCommentsCacheFileStr)):
+        cache = open(scrapedCommentsCacheFileStr, "r")
+        scrapedComments = json.loads(cache.read())
+    else:
+        scrapedComments = scrape(userid)
+        # Cache
+        cache = open(scrapedCommentsCacheFileStr, "w")
+        cache.write(json.dumps(scrapedComments))
+    cache.close()
+    
+    return commentAndIdToTagSentiment(scrapedComments)
 
 #scrape("dstein64")
 #print commentAndIdToTagSentiment(scrape("vijaydev"))
 #print scrape("dstein64")
 #print commentAndIdToTagSentiment(scrape("melissamiranda"))
 #print commentAndIdToTagSentiment(scrape("edw519"))
-#print commentAndIdToTagSentiment(scrape("pg"))
+#print getUserTopicSentiments("dstein64")
